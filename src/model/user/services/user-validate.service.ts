196112f3ab210e5a09259016b9ed0d63609f7e7c
idooru <shere1765@gmail.com>
2025-05-17 20:42:00 +0900

    [setup] 사용자 필드 유효성 검사 개선

--- src/model/user/services/user-validate.service.ts
+++ src/model/user/services/user-validate.service.ts
@@ -11,6 +11,8 @@ export class UserValidateService {
       return { isValidate: true, message: "" };
     } else if (!currentNickName.length) {
       return { isValidate: false, message: "입력된 내용이 없습니다." };
+    } else if (currentNickName.includes(" ")) {
+      return { isValidate: false, message: "공백을 포함할 수 없습니다." };
     } else if (currentNickName.length > 20) {
       return { isValidate: false, message: "길이가 20자를 넘어갑니다." };
     } else if (await this.repository.validateNickName(currentNickName)) {
@@ -24,16 +26,14 @@ export class UserValidateService {
     beforePhoneNumber: string,
     currentPhoneNumber: string,
   ): Promise<ResponseValidateDto> {
-    const phoneNumberReg = /^([^-\n]*-){2}[^-\n]*$/.test(currentPhoneNumber);
+    const isValidPhoneNumberReg = /^\d{3}-\d{4}-\d{4}$/.test(currentPhoneNumber);
 
     if (beforePhoneNumber == currentPhoneNumber) {
       return { isValidate: true, message: "" };
     } else if (!currentPhoneNumber.length) {
       return { isValidate: false, message: "입력된 내용이 없습니다." };
-    } else if (currentPhoneNumber.length > 15) {
-      return { isValidate: false, message: "길이가 15자를 넘어갑니다." };
-    } else if (!phoneNumberReg) {
-      return { isValidate: false, message: "전화번호 유효성이 어긋납니다. '-'를 포함시켜주세요." };
+    } else if (!isValidPhoneNumberReg) {
+      return { isValidate: false, message: "'000-0000-0000'형태를 준수해주세요." };
     } else if (await this.repository.validatePhoneNumber(currentPhoneNumber)) {
       return { isValidate: false, message: "이미 존재하는 전화번호입니다." };
     } else {
@@ -56,7 +56,7 @@ export class UserValidateService {
   }
 
   public async validateEmail(beforeEmail: string, currentEmail: string): Promise<ResponseValidateDto> {
-    const emailReg = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(currentEmail);
+    const isValidEmailReg = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(currentEmail);
 
     if (beforeEmail == currentEmail) {
       return { isValidate: true, message: "" };
@@ -64,8 +64,8 @@ export class UserValidateService {
       return { isValidate: false, message: "입력된 내용이 없습니다." };
     } else if (currentEmail.length > 25) {
       return { isValidate: false, message: "길이가 25자를 넘어갑니다." };
-    } else if (!emailReg) {
-      return { isValidate: false, message: "이메일 유효성이 어긋납니다. '@'를 포함시켜주세요." };
+    } else if (!isValidEmailReg) {
+      return { isValidate: false, message: "'email@domain'형태를 준수해주세요." };
     } else if (await this.repository.validateEmail(currentEmail)) {
       return { isValidate: false, message: "이미 존재하는 이메일입니다." };
     } else {
@@ -74,8 +74,9 @@ export class UserValidateService {
   }
 
   public async validatePassword(newPassword: string, matchPassword: string): Promise<ResponseValidateDto> {
-    const newPasswordReg = /^[A-Za-z\d!@#$%^&*()]/.test(newPassword);
-    const matchPasswordReg = /^[A-Za-z\d!@#$%^&*()]/.test(matchPassword);
+    const passwordReg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/;
+    const isValidNewPasswordReg = passwordReg.test(newPassword);
+    const isValidMatchpasswordReg = passwordReg.test(matchPassword);
 
     if (newPassword !== matchPassword) {
       return { isValidate: false, message: "입력된 비밀번호가 일치하지 않습니다." };
@@ -83,8 +84,8 @@ export class UserValidateService {
       return { isValidate: false, message: "길이가 8자를 넘기지 못합니다." };
     } else if (newPassword.length > 30 && matchPassword.length > 30) {
       return { isValidate: false, message: "길이가 30자를 넘어갑니다." };
-    } else if (!newPasswordReg && !matchPasswordReg) {
-      return { isValidate: false, message: "비밀번호 유효성이 어긋납니다. 영문, 특수문자 조합을 준수해주세요." };
+    } else if (!isValidNewPasswordReg && !isValidMatchpasswordReg) {
+      return { isValidate: false, message: "비밀번호 유효성이 어긋납니다. 영문, 숫자, 특수문자 조합을 준수해주세요." };
     } else {
       return { isValidate: true, message: "" };
     }

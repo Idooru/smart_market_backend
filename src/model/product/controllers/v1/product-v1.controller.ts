1c430e5eccf37e168e18aa0fd928e976e0e6f8ca
shlee <shere1765@gmail.com>
2025-07-27 16:08:01 +0900

    [feature] 전체 상품을 가져오는 메서드 제거

--- src/model/product/controllers/v1/product-v1.controller.ts
+++ src/model/product/controllers/v1/product-v1.controller.ts
@@ -7,8 +7,6 @@ import { ProductIdValidatePipe } from "../../pipe/exist/product-id-validate.pipe
 import { FindDetailProductSwagger } from "../../docs/product-v1-controller/find-detail-product.swagger";
 import { ProductBasicRawDto } from "../../dto/response/product-basic-raw.dto";
 import { ProductDetailRawDto } from "../../dto/response/product-detail-raw.dto";
-import { FindAllProductsDto } from "../../dto/request/find-all-products.dto";
-import { FindAllProductsSwagger } from "../../docs/product-v1-controller/find-all-products.swagger";
 import { SearchProductsDto } from "../../dto/request/search-product.dto";
 import { FindConditionalProductDto } from "../../dto/request/find-conditional-product.dto";
 
@@ -25,21 +23,6 @@ export class ProductV1Controller {
     return {
       statusCode: 200,
       message: `${name}와 일치하는 자동완성 목록을 가져옵니다.`,
-      result,
-    };
-  }
-
-  @FindAllProductsSwagger()
-  @UseInterceptors(JsonGeneralInterceptor)
-  @Get("/all")
-  public async findAllProducts(
-    @Query() query: FindAllProductsDto,
-  ): Promise<JsonGeneralInterface<ProductBasicRawDto[]>> {
-    const result = await this.productSearcher.findAllRaws(query);
-
-    return {
-      statusCode: 200,
-      message: "query 조건에 해당하는 전체 상품 정보를 가져옵니다.",
       result,
     };
   }

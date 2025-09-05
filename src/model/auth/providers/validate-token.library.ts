91f2bb43b416aebfda6a2138cc516ec8b1ae60b3
shlee <shere1765@gmail.com>
2025-09-05 18:51:28 +0900

    [feature] access token 해석중 유효하지 않을 경우 에러 발생

--- src/model/auth/providers/validate-token.library.ts
+++ src/model/auth/providers/validate-token.library.ts
@@ -1,10 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { SecurityLibrary } from "./security.library";
 import { JwtErrorHandlerLibrary } from "src/model/auth/providers/jwt-error-handler.library";
 import { JwtAccessTokenPayload } from "../jwt/jwt-access-token-payload.interface";
 import { JwtRefreshTokenPayload } from "../jwt/jwt-refresh-token-payload.interface";
 import { UserUpdateRepository } from "src/model/user/api/v1/repositories/user-update.repository";
+import { JwtException } from "../../../common/errors/jwt.exception";
 
 @Injectable()
 export class ValidateTokenLibrary {
@@ -15,6 +16,10 @@ export class ValidateTokenLibrary {
     private readonly userUpdateRepository: UserUpdateRepository,
   ) {}
 
+  /*
+   * accessToken을 검증하고 payload를 반환한다.
+   * 하지만 유효시간이 지난 토큰이라면 에러가 발생한다.
+   * */
   public async validateAccessToken(accessToken: string): Promise<JwtAccessTokenPayload> {
     return await this.jwtService
       .verifyAsync(accessToken, this.securityLibrary.jwtAccessTokenVerifyOption)
@@ -30,7 +35,23 @@ export class ValidateTokenLibrary {
       });
   }
 
+  /*
+   * accessToken의 payload만 반환한다.
+   * 따라서 유효시간이 지난 토큰이라도 payload 반환이 가능하다.
+   * */
   public async decodeAccessToken(accessToken: string): Promise<JwtAccessTokenPayload> {
-    return this.jwtService.decode(accessToken) as JwtAccessTokenPayload;
+    const accessTokenPayload = (await this.jwtService.decode(accessToken)) as JwtAccessTokenPayload;
+
+    if (!accessTokenPayload) {
+      const message = "변조된 jwt payload 입니다.";
+      throw new JwtException({
+        name: "JsonWebTokenError",
+        message,
+        whatToken: "access_token",
+        status: HttpStatus.UNAUTHORIZED,
+      });
+    }
+
+    return accessTokenPayload;
   }
 }
